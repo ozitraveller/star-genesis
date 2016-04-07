fd7fde3f0af90c3dad7a7f08af3aa3d3346db339
Simon <simon@debian-7.3-64>
2016-04-08 05:50:15 +1000

    8.4 updates - dmenu

---
+++ ob-64/config/includes.chroot/etc/skel/.config/dmenu/dmenu-bind.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+dmenu_run -b -i -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617'
+# exe=`dmenu_path | dmenu_run -nb '#000000' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#000000'` && eval "exec $exe"
